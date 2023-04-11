9d0458f01bf4c41120f43f83dbce24eefccb5bd0
CHAWLASIMRAN123 <chawlasimran708@gmail.com>
2023-04-11 11:08:36 -0400

    Final execution
    execution of quiz app

--- quizapp/quizapp/QuestionViewController.swift
+++ quizapp/quizapp/QuestionViewController.swift
@@ -7,23 +7,53 @@
 
 import UIKit
 
-class QuestionViewController: UIViewController {
+    class QuestionViewController: UIViewController {
     
 
     @IBOutlet var multipleStackView: UIStackView!
     
+    @IBOutlet var mulLabel1: UILabel!
+    
+    @IBOutlet var mulLabel2: UILabel!
+    
+    @IBOutlet var mulLabel3: UILabel!
+    
+    @IBOutlet var mulLabel4: UILabel!
+    
     
     @IBOutlet var singleStackView: UIStackView!
     
+    @IBOutlet var singleButton1: UIButton!
+    
+    @IBOutlet var singleButton2: UIButton!
+    
+    @IBOutlet var singleButton3: UIButton!
+    
+    @IBOutlet var singleButton4: UIButton!
+    
+    
     @IBOutlet var rangedStackView: UIStackView!
-    var question: [Question] = [
-        Question(text: "Which food do you like the most?", type: .single, answers: [
+    
+    
+    @IBOutlet var rangedLabel1: UILabel!
+    
+    @IBOutlet var rangedLabel2: UILabel!
+    
+    @IBOutlet var questionProgressview: UIProgressView!
+    
+    @IBOutlet var questionLabel: UILabel!
+    
+    var questions: [Question] = [
+        Question(text: "Which food do you like the most?",
+            type: .single,
+            answers: [
             Answer(text: "Steak", type: .dog),
             Answer(text: "Grains", type: .fish),
             Answer(text: "Carrots", type: .rabbit),
             Answer(text: "Corn", type: .turtle)
         ]),
-        Question(text: "Which activities do you enjoy?", type: .multiple, answers: [
+        Question(
+            text: "Which activities do you enjoy?", type: .multiple, answers: [
             Answer(text: "Swimming", type: .turtle),
             Answer(text: "Sleeping", type: .fish),
             Answer(text: "Cuddling", type: .rabbit),
@@ -39,37 +69,170 @@ class QuestionViewController: UIViewController {
         ])
     ]
     
-    var questionIndex = 0
-
+        var questionIndex = 0
+        var answersChosen: [Answer] = []
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateUI()
     }
     
-    func updateUI() {
-        singleStackView.isHidden = true
-        multipleStackView.isHidden = true
-        rangedStackView.isHidden = true
         
-        navigationItem.title = "Question #\(questionIndex + 1)"
         
-        let currentQuestion = question[questionIndex]
         
-        switch currentQuestion.type {
-        case .single:
-            singleStackView.isHidden = false
-        case .multiple:
-            multipleStackView.isHidden = false
-        case .ranged:
-            rangedStackView.isHidden = false
+
+
+        
+    
+
+        @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        
+        
+        let currentAnswers = questions[questionIndex].answers
+            
+            switch sender {
+            case singleButton1:
+                answersChosen.append(currentAnswers[0])
+            case singleButton2:
+                answersChosen.append(currentAnswers[1])
+            case singleButton3:
+                answersChosen.append(currentAnswers[2])
+            case singleButton4:
+                answersChosen.append(currentAnswers[3])
+            default:
+                break
+            }
+            nextQuestion()
             
         }
         
-    }
+        @IBOutlet var multiSwitch1: UISwitch!
+        
+        @IBOutlet var multiSwitch2: UISwitch!
+        
+        @IBOutlet var multiSwitch3: UISwitch!
+        
+        @IBOutlet var multiSwitch4: UISwitch!
+        
+        
+        @IBAction func multipleAnswerButtonPressed() {
+            let currentAnswers = questions[questionIndex].answers
+            
+            if multiSwitch1.isOn {
+                answersChosen.append(currentAnswers[0])
+            }
+            if multiSwitch2.isOn {
+                answersChosen.append(currentAnswers[1])
+            }
+            if multiSwitch3.isOn {
+                answersChosen.append(currentAnswers[2])
+            }
+            if multiSwitch4.isOn {
+                answersChosen.append(currentAnswers[3])
+            }
+            nextQuestion()
+        }
+        
+        @IBOutlet var rangedSlider: UISlider!
+        
+        
+        @IBAction func rangedAnswerButtonPressed() {
+            let currentAnswers = questions[questionIndex].answers
+            let index = Int(round(rangedSlider.value *
+                Float(currentAnswers.count - 1)))
+            answersChosen.append(currentAnswers[index])
+            
+            nextQuestion()
+            
+            
+        }
+        func nextQuestion() {
+            questionIndex += 1
+            
+            if questionIndex < questions.count
+            {
+                updateUI()
+            } else
+            {
+                performSegue(withIdentifier: "Results", sender: nil)
+            }
+        }
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+        {
+            if segue.identifier == "Results"
+            {
+                if let dest = segue.destination as? ResultsViewController
+                {
+                    dest.responses = answersChosen
+                }
+            }
+        }
+        
+        func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+    
+        
+        let currentQuestion = questions[questionIndex]
+        let currentAnswers = currentQuestion.answers
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        navigationItem.title = "Question #\(questionIndex + 1)"
+        questionLabel.text = currentQuestion.text
+        questionProgressview.setProgress(totalProgress, animated: true)
+        
+        switch currentQuestion.type {
+            
+        case .single:
+            updateSingleStack(using: currentAnswers)
+        case .multiple:
+            updateMultipleStack(using: currentAnswers)
+        case .ranged:
+            updateRangedStack(using: currentAnswers)
+            
+        }
 
+    }
+        
+        func updateSingleStack(using answers: [Answer]) {
+            singleStackView.isHidden = false
+            singleButton1.setTitle(answers[0].text, for: .normal)
+            singleButton2.setTitle(answers[1].text, for: .normal)
+            singleButton3.setTitle(answers[2].text, for: .normal)
+            singleButton4.setTitle(answers[3].text, for: .normal)
+            
+        }
+        
+        func updateMultipleStack(using answers: [Answer]) {
+            multipleStackView.isHidden = false
+            multiSwitch1.isOn = false
+            multiSwitch2.isOn = false
+            multiSwitch3.isOn = false
+            multiSwitch4.isOn = false
+            mulLabel1.text = answers[0].text
+            mulLabel2.text = answers[1].text
+            mulLabel3.text = answers[2].text
+            mulLabel4.text = answers[3].text
+            
+        }
+        func updateRangedStack(using answers: [Answer]) {
+            rangedStackView.isHidden = false
+            rangedSlider.setValue(0.5, animated: false)
+            rangedLabel1.text = answers.first?.text
+            rangedLabel2.text = answers.last?.text
+            
+        }
+        
+        
+        
+        @IBSegueAction func showResults(_ coder: NSCoder) -> ResultsViewController? {
+            return ResultsViewController(coder: coder, responses: answersChosen)
+        }
+    
+    
     /*
     // MARK: - Navigation
 
--- quizapp/quizapp/ViewController.swift
+++ quizapp/quizapp/ViewController.swift
@@ -1,14 +1,12 @@
-//
-//  ViewController.swift
-//  quizapp
-//
-//  Created by Canadore Student on 2023-03-28.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
